3ac1d17c2ea23818db03f1080ef88529fae79754
Acerbi <lgacerbi@gmail.com>
2021-10-09 12:57:51 -0300

    test: function name

--- tests/geojson-scanner.test.js
+++ tests/geojson-scanner.test.js
@@ -4,11 +4,11 @@ const { polygonWithLoop, polygonWithoutLoop } = require('./mocks/polygons')
 describe('GeoJson Scanner tests', () => {
     describe('Scann loop in a polygon tests', () => {
         test('should return true, to a polygon with loop', () => {
-            expect(GeoJsonScanner.geometryLoop(polygonWithLoop)).toBe(true)
+            expect(GeoJsonScanner.polygonLoop(polygonWithLoop)).toBe(true)
         })
 
         test('should return false, to a polygon without loop', () => {
-            expect(GeoJsonScanner.geometryLoop(polygonWithoutLoop)).toBe(false)
+            expect(GeoJsonScanner.polygonLoop(polygonWithoutLoop)).toBe(false)
         })
     })
 })
