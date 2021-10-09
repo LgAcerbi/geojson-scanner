3908ff3013fe50366aea2fb8ba53a1be224ca5da
Acerbi <lgacerbi@gmail.com>
2021-10-09 15:42:30 -0300

    test: validations and scanner

---
+++ tests/mocks/coordinates.js
@@ -0,0 +1,21 @@
+module.exports = {
+    validCoordinate: [
+        -45.4133,
+        -7.297087564171992
+    ],
+    invalidLongitudeCoordinate: [
+        -1000,
+        -7
+    ],
+    invalidLatitudeCoordinate: [
+        -7,
+        -1000
+    ],
+
+    invalidCoordinateByValuesTypes: [
+        "3123",
+        "adw"
+    ],
+
+}
+
