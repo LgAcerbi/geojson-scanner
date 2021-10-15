01780861e95f37c96bd569807aab05a8dce55204
Acerbi <lgacerbi@gmail.com>
2021-10-15 20:13:29 -0300

    feat: expose validation functions

--- src/geojson-scanner.js
+++ src/geojson-scanner.js
@@ -1,7 +1,23 @@
 const GeoJsonValidations = require('./helpers/geojson-validations')
-const { checkIntersectionBetweenLines } = require('./helpers/geo-math')
+const GeoMath = require('./helpers/geo-math')
 
 class GeoJsonScanner {
+    static isValidPolygon(polygon) {
+        try {
+            return GeoJsonValidations.isValidPolygon(polygon)
+        } catch (error) {
+            console.log(error)
+            return false
+        }
+    }
+    static isValidCoordinate(coordinate) {
+        try {
+            return GeoJsonValidations.isValidCoordinate(coordinate)
+        } catch (error) {
+            console.log(error)
+            return false
+        }
+    }
     static polygonLoop(polygon) {
         GeoJsonValidations.isValidPolygon(polygon)
         const foundLoop = polygon.coordinates[0].some((coordinate, index) => {
@@ -15,7 +31,7 @@ class GeoJsonScanner {
                     coordA: polygon.coordinates[0][index + 2][0], coordB: polygon.coordinates[0][index + 2][1],
                     coordC: polygon.coordinates[0][index + 3][0], coordD: polygon.coordinates[0][index + 3][1]
                 }
-                if (checkIntersectionBetweenLines(line1, line2)) return true;
+                if (GeoMath.findIntersectionBetweenLines(line1, line2)) return true;
             }
         })
         return foundLoop
