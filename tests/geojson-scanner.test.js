a4c1aa78ad3deb67a5d4c0b5079ce60391710ace
Acerbi <lgacerbi@gmail.com>
2021-10-15 20:43:56 -0300

    test: Scanner validation tests

--- tests/geojson-scanner.test.js
+++ tests/geojson-scanner.test.js
@@ -1,5 +1,6 @@
 const GeoJsonScanner = require('../src/geojson-scanner')
 const polygonsMock = require('./mocks/polygons')
+const coordinatesMock = require('./mocks/coordinates')
 
 describe('GeoJson Scanner tests', () => {
     describe('Scan loop in a polygon tests', () => {
@@ -9,6 +10,26 @@ describe('GeoJson Scanner tests', () => {
 
         test('should return false, to a polygon without loop', () => {
             expect(GeoJsonScanner.polygonLoop(polygonsMock.polygonWithoutLoop)).toBe(false)
+        })
+    })
+
+    describe('Validate polygon tests', () => {
+        test('should return true to a valid polygon', () => {
+            expect(GeoJsonScanner.isValidPolygon(polygonsMock.polygonWithoutLoop)).toBe(true)
+        })
+
+        test('should return false to an invalid polygon', () => {
+            expect(GeoJsonScanner.isValidPolygon(polygonsMock.polygonWithInvalidCoordinates)).toBe(false)
+        })
+    })
+
+    describe('Validate coordinate tests', () => {
+        test('should return true, to a valid coordinate', () => {
+            expect(GeoJsonScanner.isValidCoordinate(coordinatesMock.validCoordinate)).toBe(true)
+        })
+
+        test('should return false, to an invalid coordinate', () => {
+            expect(GeoJsonScanner.isValidCoordinate(coordinatesMock.invalidLatitudeCoordinate)).toBe(false)
         })
     })
 })
