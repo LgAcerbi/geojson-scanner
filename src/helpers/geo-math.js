9c3115815542c612cc36a8c5aec4157a8ce6bb17
Acerbi <lgacerbi@gmail.com>
2021-10-16 15:32:27 -0300

    feat: calculate distance between points

--- src/helpers/geo-math.js
+++ src/helpers/geo-math.js
@@ -1,14 +1,28 @@
 class GeoMath {
   static findIntersectionBetweenLines(line1, line2) {
-    let det, gamma, lambda;
-    det = (line1.coordC - line1.coordA) * (line2.coordD - line2.coordB) - (line2.coordC - line2.coordA) * (line1.coordD - line1.coordB);
+    const det = (line1.coordC - line1.coordA) * (line2.coordD - line2.coordB) - (line2.coordC - line2.coordA) * (line1.coordD - line1.coordB);
     if (det === 0) {
       return false;
     } else {
-      lambda = ((line2.coordD - line2.coordB) * (line2.coordC - line1.coordA) + (line2.coordA - line2.coordC) * (line2.coordD - line1.coordB)) / det;
-      gamma = ((line1.coordB - line1.coordD) * (line2.coordC - line1.coordA) + (line1.coordC - line1.coordA) * (line2.coordD - line1.coordB)) / det;
+      const lambda = ((line2.coordD - line2.coordB) * (line2.coordC - line1.coordA) + (line2.coordA - line2.coordC) * (line2.coordD - line1.coordB)) / det;
+      const gamma = ((line1.coordB - line1.coordD) * (line2.coordC - line1.coordA) + (line1.coordC - line1.coordA) * (line2.coordD - line1.coordB)) / det;
       return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
     }
+  }
+
+  static calculateDistanceBetweenPointsInMetres(point1, point2) {
+    const earthRadiusInMetres = 6371e3;
+    const point1LatInRadians = point1.lat * Math.PI / 180;
+    const point2LatInRadians = point2.lat * Math.PI / 180;
+    const latDeltaInRadians = (point2LatInRadians - point1LatInRadians) * Math.PI / 180;
+    const longDeltaInRadians = (point2.long - point1.long) * Math.PI / 180;
+
+    const a = Math.sin(latDeltaInRadians / 2) * Math.sin(latDeltaInRadians / 2) +
+      Math.cos(point1LatInRadians) * Math.cos(point2LatInRadians) *
+      Math.sin(longDeltaInRadians / 2) * Math.sin(longDeltaInRadians / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return earthRadiusInMetres * c;
   }
 }
 
